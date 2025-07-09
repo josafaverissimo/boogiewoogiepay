2e0aafe7845b8ab8498582021e02c6af60b34ce2
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-07-09 02:07:39 -0300

    feat(Utils): shared utils added

---
+++ app/src/main/java/com/josafaverissimo/boogiewoogiepay/shared/Utils.java
@@ -0,0 +1,10 @@
+package com.josafaverissimo.boogiewoogiepay.shared;
+
+public final class Utils {
+  private Utils() {
+  }
+
+  public static boolean isStrEmpty(String str) {
+    return str == null || str == "";
+  }
+}
