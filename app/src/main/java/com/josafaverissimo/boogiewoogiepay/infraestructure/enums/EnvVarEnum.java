2cfae91fde890953706b31f5f0e4fcb9df6ba2da
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-07-09 23:16:19 -0300

    refactor(AppEnv): get env vars by get static method

---
+++ app/src/main/java/com/josafaverissimo/boogiewoogiepay/infraestructure/enums/EnvVarEnum.java
@@ -0,0 +1,7 @@
+package com.josafaverissimo.boogiewoogiepay.infraestructure.enums;
+
+public enum EnvVarEnum {
+  PAYMENT_PROCESSOR_URL_DEFAULT,
+  PAYMENT_PROCESSOR_URL_FALLBACK,
+  X_RINHA_TOKEN;
+}
