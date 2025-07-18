ba38d1ad3eb4126e44e6b3abf968aecbba9a331e
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2025-07-18 08:13:41 -0300

    wip: trying to compile and run with graalvm

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -1,6 +1,10 @@
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+
 plugins {
   application
   id("com.gradleup.shadow") version "8.3.8"
+  id("org.graalvm.buildtools.native") version "0.10.6"
 }
 
 repositories {
@@ -22,6 +26,7 @@ dependencies {
 java {
   toolchain {
     languageVersion = JavaLanguageVersion.of(24)
+    vendor = JvmVendorSpec.ORACLE
   }
 }
 
@@ -29,7 +34,39 @@ application {
   mainClass = "com.josafaverissimo.worker.Worker"
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
+graalvmNative {
+  agent {
+    enabled.set(true)
+    metadataCopy {
+      inputTaskNames.add("run")
+      outputDirectories.add("src/main/resources/META-INF/native-image/com.josafaverissimo.worker")
+      mergeWithExisting.set(true)
+    }
+  }
 }
 
+tasks.named<Test>("test") {
+  useJUnitPlatform()
+}
+
+tasks.register("copyMetadata") {
+  val agentOutput = file("build/native/agent-output")
+  val destinationFile = file("src/main/resources/META-INF/native-image/reachability.json")
+
+  doLast {
+    val latestFile = agentOutput.walkTopDown()
+    .filter { it.isFile && it.name == "reachability-metadata.json" }
+    .maxByOrNull {
+      Files.readAttributes(it.toPath(), BasicFileAttributes::class.java).lastModifiedTime()
+    }
+
+    if (latestFile != null) {
+      println("Copying reachability-metadata.json from: ${latestFile.absolutePath}")
+      destinationFile.parentFile.mkdirs()
+      latestFile.copyTo(destinationFile, overwrite = true)
+      println("Paste into: ${destinationFile.absolutePath}")
+    } else {
+      println("No reachability-metadata.json file found in build/native/agent-output")
+    }
+  }
+}
